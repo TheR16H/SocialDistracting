629adc446b20be9813b825907aebbb8a5e7d5510
TheR16H <rashawn5hall@gmail.com>
2024-09-12 14:00:09 -0400

    users/thoughts/friends/jsons/sever.js/stuff

--- models/users.js
+++ models/users.js
@@ -13,12 +13,25 @@ email: {
     required: true,
     match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,  "Your email was wrong, please enter a valid email address"]
 },
-// * `thoughts`
-//   * Array of `_id` values referencing the `Thought` model
-
-// * `friends`
-//   * Array of `_id` values referencing the `User` model (self-reference)
-// **Schema Settings**:
+thoughts: [
+    {
+       type: Schema.Types.ObjectId, 
+       ref: "Thoughts"
+    },
+],
+friends:[
+    {
+        type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
 
 // Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
-})
+});
