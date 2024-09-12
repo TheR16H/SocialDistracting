24f3a81c48f2f11b6f05450ff8e4deb32ed5dab4
TheR16H <rashawn5hall@gmail.com>
2024-09-12 15:46:55 -0400

    with help continued the CRUD routes inside the user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,36 @@
-const {Thoughts, User} = require("../models")
+const { Thought, User } = require("../models");
 
 const userController = {
-    getAllUsers(req, res){
-        Users.find()
-        .then((users) => res.json(users))
-    }
+    getAllUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    createUser(req, res){
+        User.create(req.body)
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res){
+        User.findOneAndUpdate(
+            {_id:req.params.id},
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => {
+            !user ? res.status(404).json({ message: 'No user' }) : res.json(user);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.id })
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user with that ID' });
+                }
+                return Thought.deleteMany({ _id: { $in: user.thoughts } });
+            })
+            .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
 }
