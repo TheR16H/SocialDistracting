4c1356fb40508031c002d5df36e938cccb5164c6
TheR16H <rashawn5hall@gmail.com>
2024-09-12 16:02:59 -0400

    with help networking we finished controller for the user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const { Thought, User } = require("../models");
-
-const userController = {
+// bug on line 33?
+const userController = { 
     getAllUsers(req, res) {
         User.find()
             .then((users) => res.json(users))
@@ -33,4 +33,29 @@ const userController = {
             .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
+    getUserById(req, res){
+        User.findOne({_id: req.params})
+        .then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user))
+        .catch((err) => res.status(500).json(err));
+},
+addFriend(req, res){
+    User.findOneAndUpdate(
+        {_id: ewq.params.id},
+        {$addToSet: { friends: req.params.friendsid } },
+        {runValidators: true, new: true}
+    )
+    .then((user) => !user ? res.status(404).json({ message: 'No friend found with that ID :(' }) : res.json(user))
+    .catch((err) => res.status(500).json(err));
+},
+removeFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $pull: { friends: req.params.friendsId } },
+        { runValidators: true, new: true }
+    )
+    .then((user) => !user ? res.status(404).json({ message: 'No friend found with that ID :(' }) : res.json(user))
+    .catch((err) => res.status(500).json(err));
 }
+};
+
+module.exports = userController;
